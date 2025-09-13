3c56fea1c5f413090098bc2bb925a41d426c5e1d
adviislam10 <sahiislam4@gmail.com>
2025-09-13 11:14:03 -0600

    initial backend

--- ApplicationTracker/mainapp/models.py
+++ ApplicationTracker/mainapp/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Application(models.Model):
-    company_name = models.CharField(max_length=100)
+    STATUS_CHOICES = [
+        ("applied", "Applied"),
+        ("interviewed", "Interviewed"),
+        ("submitted", "Submitted")
+    ]
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="applied")
+    resume_text = models.TextField(blank=True)
     date_applied = models.DateTimeField()
-    
+
+    def __str__(self):
+        return f"{self.company} - {self.position} ({self.status})"
