7793691d0eecfa1cc15b4523ba16cb2ab770acb0
adviislam10 <sahiislam4@gmail.com>
2025-09-14 11:56:18 -0600

    added sample data

--- ApplicationTracker/mainapp/models.py
+++ ApplicationTracker/mainapp/models.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_migrate
 
 # Create your models here.
 class Application(models.Model):
@@ -15,3 +18,21 @@ class Application(models.Model):
 
     def __str__(self):
         return f"{self.company} - {self.position} ({self.status})"
+    
+@receiver(post_migrate)
+def create_default_applications(sender, **kwargs):
+    if sender.name == 'mainapp' and not Application.objects.exists():
+            Application.objects.create(
+                company="Google",
+                position="Software Engineer",
+                status="applied",
+                resume_text="This is a sample resume text.",
+                date_applied=timezone.now()
+            )
+            Application.objects.create(
+                company="Microsoft",
+                position="Data Scientist",
+                status="interviewed",
+                resume_text="This is another sample resume text.",
+                date_applied=timezone.now()
+            )
